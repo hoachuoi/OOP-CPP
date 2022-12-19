fa4af4519d7ce4e20f4a67b7c86a4d13d755decd
hoachuoi <76391029+hoachuoi@users.noreply.github.com>
2022-12-19 22:55:48 +0700

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+ //   string s;
+    int n;
+    cin>> n;
+   // for (char x : s){
+   //     cout<<x;
+    //};
+    for(int i=0;i<10;i++){
+        cout<<i;
+    }
+    return 0;
+}
